409b5a3fe8ad43c7eca19a01c7db4e64b702dfb0
Navid Hallajian <navidhg@gmail.com>
2014-03-24 09:59:00 +0000

    Added connection to Arduino

--- Hardware/tcpserver.py
+++ Hardware/tcpserver.py
@@ -1,8 +1,10 @@
 # TCP server example
 import socket
+import serial
 import time
 from random import randint
 
+ser = serial.Serial('/dev/tty.usbmodem1411', 9600)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("localhost", 5005))
 server_socket.listen(5)
@@ -12,7 +14,8 @@ print "TCPServer Waiting for client on port 5004"
 while 1:
 	client_socket, address = server_socket.accept()
 	print "I got a connection from ", address
-	i = 1
+	i = 1 # i is used for mock data. Actual data comes from Arduino in next line
+	arduino_data = ser.readline()
 	while 1:
 		i = i+1
 		data = str(i)
